95998c083d6f6bdc38f44b16cd232eaf284d93cd
Jiyuan Lin <linjiyuan90@gmail.com>
2013-07-15 19:17:04 +0800

    add c++/async

---
+++ C++/The_C++_Reference_Library/concurrency/async1.cc
@@ -0,0 +1,44 @@
+
+#include "future"
+#include "thread"
+#include "chrono"
+#include "random"
+#include "iostream"
+#include "exception"
+
+int do_something(char c) {
+  // random-number generator (use c as seed to get different sequences)
+  std::default_random_engine dre(c);
+  std::uniform_int_distribution<int> id(1000, 10000);
+
+  // loop to print character after a random period of time
+  for (int i = 0; i < 10; i++) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(id(dre)));
+    std::cout.put(c).flush();
+  }
+  return c;
+}
+
+int func1() {
+  return do_something('.');
+}
+
+int func2() {
+  return do_something('+');
+}
+
+int main() {
+  std::cout << "Starting func1() in background"
+	    << " and func2() in foreground: " << std::endl;
+
+  // start func1() asychronously (now or later or never):
+  std::future<int> result1(std::async(func1));
+
+  int result2 = func2();  // call func2() synchronously (here and now)
+
+  // print result (wait for func1() to finish and add its result to result2
+  int result = result1.get() + result2;
+  
+  std::cout << "\n result of func1() + func2(): " << result
+	    << std::endl;
+}
